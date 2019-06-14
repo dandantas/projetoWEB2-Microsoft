e1cdb491a9dd9d73b0c528586c627ff56fa268f0
Daniel Dantas Marotti <dantasmaarotti@gmail.com>
2019-06-14 05:52:19 -0300

    using cookies and some changing in templates/routes

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,10 +1,12 @@
 let bodyParser = require('body-parser'),
     path = require('path'),
-    express = require('express');
+    express = require('express'),
+    cookieParser = require('cookie-parser');
 
 module.exports = app => {
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-
+    app.use(cookieParser());
     
     app.use(express.static('public'));
     app.set('views', path.join(__dirname, '../views'));
--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,8 +1,54 @@
+let userDao = require('../libs/database');
+var express = require('express');
+var router = express.Router();
 
-module.exports = app=>{
+module.exports = router=>{
+    
+    router.get('/', (req, res,next)=>{
+        
+        if(req.cookies && req.cookies.login){
+            res.render('home', {
+                user: req.cookies.login
+            });
+            console.log('pagina admin');
+            return;
+        }else{
+            res.render('home');
+        }
+    });
 
-    app.get('/', (req, res)=>{
-        res.render('home');
+
+    router.get('/register', (req,res)=>{
+        
+            res.render('register');
+            
     });
     
+
+    router.post('/register', (req,res)=>{
+        console.log(req.body);
+        userDao.insert(req.body);
+        res.redirect('/');
+        res.end();
+    });
+
+    router.get('/login', (req,res,next)=>{
+        res.render('home', {});
+    });
+
+    router.post('/login', (req,res,next)=>{
+        let login = req.body.login,
+            psw   = req.body.psw;
+        if(login === 'admin' && psw === '1234'){
+            res.cookie('login', 'admin');
+            res.redirect('/');
+            return;
+        }else{
+            res.status(403);
+            res.redirect('register');
+            res.end();
+        }
+    });
+
+
 }
